8844ee818858d7074e7c0bb98430f91360af22cb
Pranjal Bansal <pranban12@gmail.com>
2021-06-20 12:02:44 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+const app =express();
+
+app.get("/",function(req,res) {
+var today = new Date();
+if(today.getDay()===6 || today.getDay===0) {
+res.write("Yay! Its the weekend");
+}
+else{
+res.sendFile(__dirname +"/index.html");
+}
+});
+
+app.listen(3000,function() {
+console.log("Server started on port 3000");
+});
